738d504a19c3e16da94d3b3d1bf7c1ca8b9d4f94
sàm <nek0zyx@ismyuser.name>
2025-02-01 21:27:59 +0100

    Worked on it a bit more

--- Einrichter-in-chroot.sh
+++ Einrichter-in-chroot.sh
@@ -1935,11 +1935,81 @@ function eic.rpm.install() {
 		# Extract needed packages for compiling RPM
 		tar -xvf rpm*.bz
 		mv rpm-4.20.0 rpm
-		tar -xvf
+		tar -xvf cmake*.tar.gz
+		mv cmake-3.31.5 cmake
+		tar -xvf debugedit*.tar.xz
+		mv debugedit-0.3 debugedit
+		tar -xvf lua*.gz
+		mv lua-5.4.7 lua
+		pushd cmake/
+			sed -i '/"lib64"/s/64//' Modules/GNUInstallDirs.cmake
+			./bootstrap --prefix=/usr        \
+			            --system-libs         \
+			            --mandir=/share/man    \
+			            --no-system-jsoncpp     \
+			            --no-system-cppdap       \
+			            --no-system-librhash      \
+			            --docdir=/share/doc/cmake-3.31.5
+			make
+			make install
+		popd
+		pushd debugedit/
+			./configure --prefix=/usr 
+			make
+			make install
+		popd
+		pushd lua/
+			cat > lua.pc << "EOF"
+V=5.4
+R=5.4.7
+
+prefix=/usr
+INSTALL_BIN=${prefix}/bin
+INSTALL_INC=${prefix}/include
+INSTALL_LIB=${prefix}/lib
+INSTALL_MAN=${prefix}/share/man/man1
+INSTALL_LMOD=${prefix}/share/lua/${V}
+INSTALL_CMOD=${prefix}/lib/lua/${V}
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${prefix}/include
+			
+Name: Lua
+Description: An Extensible Extension Language
+Version: ${R}
+Requires:
+Libs: -L${libdir} -llua -lm -ldl
+Cflags: -I${includedir}
+EOF
+			patch -Np1 -i ../lua-5.4.7-shared_library-1.patch
+			make linux
+			make all install
+			make INSTALL_TOP=/usr                \
+			     INSTALL_DATA="cp -d"            \
+			     INSTALL_MAN=/usr/share/man/man1 \
+			     TO_LIB="liblua.so liblua.so.5.4 liblua.so.5.4.7" \
+			     install &&
+			
+			mkdir -pv                      /usr/share/doc/lua-5.4.7 &&
+			cp -v doc/*.{html,css,gif,png} /usr/share/doc/lua-5.4.7 &&
+			
+			install -v -m644 -D lua.pc /usr/lib/pkgconfig/lua.pc
+		popd
+	popd
+}
+
+function eic.rpm.install.fr() {
+	pushd /sources/rpm
+		mkdir _build
+		cd _build
+		cmake ..
+		make
+		make install
+	popd
 }
 
 function eic.eko.install() {
-	
+	echo "Not implemented"
 }
 
 function eic.signoff() {
